07a203d8def5470ab0229f6822d45beeae689811
sereta.harmand <sereta.harmand@MacBook-Pro-de-Sereta.local>
2026-03-31 23:32:13 +0200

    [Feature] Close modal - work in progress

--- FrontEnd/assets/scripts/modal.js
+++ FrontEnd/assets/scripts/modal.js
@@ -1,7 +1,26 @@
+// import { getWorks, displayWorks } from "./script.js";
+
 const editingBtn = document.querySelector('.container-editing');
-const modal = document.querySelector('.modal-bg');
+const modalBackground = document.querySelector('.modal-bg');
+const xmark = document.getElementById('xmark');
+// const editingGallery = document.querySelector('.editing-gallery');
 
 // Open modal
 editingBtn.addEventListener("click", (event) => {
-    modal.style.display = "block";
+    modalBackground.style.display = "block";
 });
+
+// Close modal - click on xmark icon
+xmark.addEventListener("click", (event) => {
+    modalBackground.style.display = "none";
+});
+
+// Close modal - click on modal background
+modalBackground.addEventListener("click", (event) => {
+    if (event.target === modalBackground && modalBackground.style.display === "block") {
+        modalBackground.style.display = "none";
+    };
+});
+
+// getWorks();
+// displayWorks(editingGallery);
